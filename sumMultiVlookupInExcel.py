8e642e34989e70acdc228d29c08f2ad32955f767
naltosi-i <sol.naltosi@gmail.com>
2022-04-14 12:54:12 +0900

    output_line_fixed

--- sumMultiVlookupInExcel.py
+++ sumMultiVlookupInExcel.py
@@ -1,9 +1,16 @@
 import os
+from datetime import datetime
 
-s= 'test'
+now = datetime.now()
+str_now = f'{now:%Y-%m-%d_%H:%M:%S}
 
-path_l = 'log' + '.txt'
+s = 'test'
 
-with open(path_l, mode='w') as f: # output text file
+log_path = '\log' + str_now +'.txt'
+
+with open(log_path, mode='a', encoding='utf-8') as f: # output text file
     f.write(s)
+
+print("End")
+
 #this programm is End
