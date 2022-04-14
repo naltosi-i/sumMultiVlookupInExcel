566b850c8afeeb8a590b3c2dcbb59fdfe6a889d9
naltosi-i <sol.naltosi@gmail.com>
2022-04-14 17:46:19 +0900

    Update sumMultiVlookupInExcel.py

--- sumMultiVlookupInExcel.py
+++ sumMultiVlookupInExcel.py
@@ -2,6 +2,27 @@ import os
 from datetime import datetime
 import func
 
+s = 'test'
+
+''' 基本構文
+IF(ISNUMBER(開始セル)*1, IFERROR(VLOOKUP(TEXT(開始セル, "@"),検索範囲,戻り値列,FALSE), 0), 0) + …
+
+開始セルに数値が入っていた場合、検索範囲から、開始セルの値を検索し、戻り値列の値を返す。
+次に隣の列の数値を検索し、戻り値列の値を加える。
+以下目的の列まで同様の処理を行う。
+'''
+
+start_column = 'G' # 開始セルの列を文字列で入力
+start_row = '10' # 開始セルの行を文字列で入力
+start_cell = start_column + start_row
+columns = 36 # VLOOKUPしたい列の数
+value = True # 検索範囲の対象が文字列の場合「True」、数値のままなら「False」
+sheet = 'レポート4!' # 検索範囲の存在するシートを記入。末尾に「!」
+search_start = '$' + 'G' # 検索範囲の開始列
+search_end = '$' + 'V' # 検索範囲の末尾列
+area = sheet + search_start + ':' + search_end
+return_column = '16' # 戻り値列（検索範囲の開始列を1とする）
+
 now = datetime.now()
 str_now = f'{now:%Y-%m-%d_%H%M%S}'
 
@@ -16,26 +37,6 @@ with open(path, mode='w', encoding='utf-8') as f:
 '''
 # 検証ここまで
 
-
-s = 'test'
-
-''' 基本構文
-IF(ISNUMBER(開始セル)*1, IFERROR(VLOOKUP(TEXT(開始セル, "@"),範囲,戻り値列,FALSE), 0), 0) + …
-
-開始セルに数値が入っていた場合、検索範囲から、開始セルの値を検索し、戻り値列の値を返す。
-次に隣の列の数値を検索し、戻り値列の値を加える。
-以下目的の列まで同様の処理を行う。
-'''
-
-start_column = 'G' # 開始セルの列を文字列で入力
-start_row = '10' # 開始セルの行を文字列で入力
-start_cell = start_column + start_row
-value = True # 検索範囲の対象が文字列の場合「True」、数値のままなら「False」
-sheet = 'レポート4!' # 検索範囲の存在するシートを記入。末尾に「!」
-search_start = '$' + 'G' # 検索範囲の開始列
-search_end = '$' + 'V' # 検索範囲の末尾列
-area = sheet + search_start + ':' + search_end
-return_column = '16' # 戻り値列（検索範囲の開始列を1とする）
 
 s = func.vLookupLine(start_cell, value, area, return_column)
 
